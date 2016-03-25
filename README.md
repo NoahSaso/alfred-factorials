a1e236f31a2e2cd03aa02eed05a7db7c4f6377b7
Noah Saso <noahsaso@gmail.com>
2016-03-24 21:29:17 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# alfred-factorials
+
+Simple Alfred 2 workflow to find the factorial of any number (<= 170).
+
+The highest it can calculate is the factorial of 170 because I didn't want to take the time to use floating point numbers and such. If you feel the need to calculate the factorials of numbers greater than that, feel free to modify this and make a pull request.
+
+NOTE: I've included the .py file to show the source code without needing to install Alfred and everything, but modifying this on your computer will NOT change the workflow. To make a pull request, you will need to modify the script in alfred, copy and paste that code into this python file, then export the modified workflow you have in Alfred and replace the one in this folder. That way, everything will be updated with the new code.
---
+++ factorial.py
@@ -0,0 +1,30 @@
+# Initial code to create list
+print('<?xml version="1.0"?>')
+print('<items>')
+print('<item uid="factorial" valid="no">')
+
+# Get arguments
+num = int("{query}")
+result = 1
+
+if num < 0:
+	print('<title>Are you crazy? No negative numbers!</title>')
+elif num == 0:
+	print('<title>0! = 1 (still debated but this works)</title>')
+else:
+	# Loop through numbers and multiply to get factorial
+	for i in range(1, num + 1):
+		result = result * i
+	# If result is bigger than 1 quintillion, put in scientific notation
+	# Also create subtitle of full number
+	if result > 1000000000000000000:
+		print('<subtitle>'+"{:,}".format(result)+'</subtitle>')
+		result = '%.2E' % result
+	# If result is not bigger than 1 quintillion, print normally
+	else:
+		result = "{:,}".format(result)
+	print('<title>'+str(num)+'! = '+result+'</title>')
+
+# Finish printing rest of code to show up
+print('</item>')
+print('</items>')
